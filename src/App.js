e47763e4655b97783fdeb131b14b824912c44705
seonga <girl0930h@naver.com>
2022-08-19 00:10:28 +0900

    localStorage구현

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import LoginPage from "./Components/LoginPage";
 import MyPage from "./Components/MyPage";
 
 const App = () => {
-  const [login, setLogin] = useState(false);
-  const [logout, setLogout] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
 
+  // 처음 렌더링 될때 로컬스토리지에 해당 키값이 있는지 확인하고 그게 value값과 같다면 로그인상태를 유지하는 useEffect
+  useEffect(()=> {
+    const storedLoggedInInfo = localStorage.getItem('isLoggedIn');
+    if(storedLoggedInInfo === '1') {
+      setIsLogin(true);
+    }
+  }, [])
 
   const onLoginHandler = () => {
-    setLogin(true);
-  
+    localStorage.setItem("isLoggedIn", '1');
+    setIsLogin(true);
   }
 
   const onLogoutHandler = () => {
-    setLogout(true);
+    localStorage.removeItem("isLoggedIn")
+    setIsLogin(false);
   }
 
 
   return (
     <React.Fragment>
-      {!login&& <LoginPage onLogin={onLoginHandler}/>}
-      {login && <MyPage login={login} onLogout={onLogoutHandler}/>}
+      {!isLogin&& <LoginPage onLogin={onLoginHandler}/>}
+      {isLogin && <MyPage onLogout={onLogoutHandler}/>}
     </React.Fragment>
   );
 }
